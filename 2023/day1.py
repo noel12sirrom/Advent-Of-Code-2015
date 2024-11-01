3d79739a07aa8206978f68b2b912c90925afe5d8
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-10-31 21:18:15 -0500

    completed day 1 part 1 for 2023

---
+++ 2023/day1.py
@@ -0,0 +1,15 @@
+with open('2023/data.txt', 'r') as data:
+    data = data.read().split("\n")
+
+total = 0
+
+for i in data:
+    tempList=[]
+    
+    for j in i:
+        if j.isdigit():
+            tempList.append(j)      
+    total += (int(tempList[0] + tempList[-1])) 
+    
+print(total)
+        
